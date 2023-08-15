9468ba59ed7c166df02d512ad5b039ecf5d8e709
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2023-08-16 00:36:53 +0800

    CI: integrate tagging system

--- build.lua
+++ build.lua
@@ -10,3 +10,41 @@ unpackfiles = { "*.dtx" }
 typesetexe = "xelatex"
 
 installfiles = { "*.sty" }
+
+function update_tag(file, content, tagname, tagdate)
+    if string.match(file, "%.dtx$") then
+        return string.gsub(content,
+            "%d%d%d%d%-%d%d%-%d%d v%d%.%d.%d%S*",
+            tagdate .. " v" .. string.gsub(tagname, "^v+", ""))
+    end
+    return content
+end
+
+function tag_hook(tagname, tagdate)
+    local orig_tagname, err_msg = io.popen("git describe --tags --abbrev=0"):read("*a")
+    if err_msg or not orig_tagname then
+        orig_tagname = '0.0.0'
+        print("Critical: Failed to fetching newest Git tag.")
+        return 1
+    end
+    orig_tagname = orig_tagname:gsub("[\n\r]", ""):gsub("^v", "")
+    tagname = string.gsub(tagname, "^v+", "")
+    print("Info: Bump version: " .. orig_tagname .. " -> " .. tagname)
+    local errorlevel = os.execute("git commit -a -m \"" ..
+        "Bump version: " .. orig_tagname .. " -> " .. tagname .. "\"")
+    if errorlevel ~= 0 then
+        print("Critical: Git commit failed.")
+        return 1
+    end
+
+    print("Info: Deleting duplicate tags:" .. tagname)
+    errorlevel = os.execute("git tag -d v" .. tagname)
+
+    print("Info: Tagging version: " .. tagname)
+    errorlevel = os.execute("git tag v" .. tagname)
+    if errorlevel ~= 0 then
+        print("Critical: Git tag failed.")
+        return 1
+    end
+    print("Info: Tagged successfully.")
+end
