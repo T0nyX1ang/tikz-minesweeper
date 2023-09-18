12bac46b2064632e8d907b6497c0e23f7b835a5b
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2023-09-18 18:03:54 +0800

    Feat: support checking translation keys

--- support/validate_translations.lua
+++ support/validate_translations.lua
@@ -12,13 +12,24 @@
 -- and version 1.3 or later is part of all distributions of
 -- LaTeX version 2005/12/01 or later.
 
-local function validate_translations (filename)
+local function validate_translations(filename)
     --[[ validate the translations of a certain dtx file ]]
 
     -- read the file
     local fin = assert(io.open(filename, "rb"), 'Failed to read file: ' .. filename)
     local lineno = 0
+    local total_translation_keys = 0
     local flag = true
+
+    local locales = { "en", "zh" }
+    local trans_table = {}
+    for _, locale in ipairs(locales) do
+        local trans_filename = "translation/" .. locale .. ".tex"
+        local trans_fin = assert(io.open(trans_filename, "rb"),
+            "Failed to read file: " .. trans_filename)
+        trans_table[locale] = trans_fin:read("*all")
+        trans_fin:close()
+    end
 
     print("Validating the translations in " .. filename .. " ...\n")
 
@@ -27,12 +38,32 @@ local function validate_translations (filename)
         -- check if the line contains undefined translations
         if line_content:match("\\TransUndefined") then
             flag = false
-            print("./" .. filename .. ":" .. lineno .. ": Undefined translation found.")
-            print("l." .. lineno .. " " .. line_content .. "\n")
+            print("./" .. filename .. ":" .. lineno ..
+                ": Undefined translation found.")
+        end
+
+        for trans_key in line_content:gmatch("\\Trans[a-zA-Z]*") do
+            total_translation_keys = total_translation_keys + 1
+            for locale, trans_content in pairs(trans_table) do
+                local key_match = false
+                for candidate_key in trans_content:gmatch("\\Trans[a-zA-Z]*") do
+                    if trans_key == candidate_key then
+                        key_match = true
+                        break
+                    end
+                end
+                if not key_match then
+                    flag = false
+                    print("./" .. filename .. ":" .. lineno ..
+                        ": Translation key " .. trans_key ..
+                        " not found for locale [" .. locale .. "].\n")
+                end
+            end
         end
     end
     fin:close()
 
+    print("Total translation keys: " .. total_translation_keys .. ".\n")
     print("Output written on stdout (0 pages).\n")
     if not flag then
         os.exit(1)
