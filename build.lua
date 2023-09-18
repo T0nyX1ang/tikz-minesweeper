77be65ac5378caed9c2d8e7e2cfd59ec1aa23078
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2023-09-18 19:06:57 +0800

    Fix: remove auto tagging feature

--- build.lua
+++ build.lua
@@ -19,32 +19,3 @@ function update_tag(file, content, tagname, tagdate)
     end
     return content
 end
-
-function tag_hook(tagname, tagdate)
-    local orig_tagname, err_msg = io.popen("git describe --tags --abbrev=0"):read("*a")
-    if err_msg or not orig_tagname then
-        orig_tagname = '0.0.0'
-        print("Critical: Failed to fetching newest Git tag.")
-        return 1
-    end
-    orig_tagname = orig_tagname:gsub("[\n\r]", ""):gsub("^v", "")
-    tagname = string.gsub(tagname, "^v+", "")
-    print("Info: Bump version: " .. orig_tagname .. " -> " .. tagname)
-    local errorlevel = os.execute("git commit -a -m \"" ..
-        "Bump version: " .. orig_tagname .. " -> " .. tagname .. "\"")
-    if errorlevel ~= 0 then
-        print("Critical: Git commit failed.")
-        return 1
-    end
-
-    print("Info: Deleting duplicate tags: " .. tagname)
-    errorlevel = os.execute("git tag -d v" .. tagname)
-
-    print("Info: Tagging version: " .. tagname)
-    errorlevel = os.execute("git tag -a v" .. tagname .. " -m \" \"")
-    if errorlevel ~= 0 then
-        print("Critical: Git tag failed.")
-        return 1
-    end
-    print("Info: Tagged successfully.")
-end
