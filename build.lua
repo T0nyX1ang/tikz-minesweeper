6157ae8f7929f84fb2adc93546d06ff4b6af697f
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2023-09-24 16:29:49 +0800

    Chore: change the tagging system

--- build.lua
+++ build.lua
@@ -14,8 +14,8 @@ installfiles = { "*.sty" }
 function update_tag(file, content, tagname, tagdate)
     if string.match(file, "%.dtx$") then
         return string.gsub(content,
-            "%d%d%d%d%-%d%d%-%d%d v%d%.%d.%d%S*",
-            tagdate .. " v" .. string.gsub(tagname, "^v+", ""))
+            "{%d%d%d%d%-%d%d%-%d%d}{%d%.%d.%d%S*}", "{" ..
+            tagdate .. "}{" .. string.gsub(tagname, "^v+", "") .. "}")
     end
     return content
 end
